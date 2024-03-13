2abffc655fc9e38bb8efdcb0e8fc8adff28135a5
FaqihAinulyaqin <mfaqihay@gmail.com>
2024-03-13 12:28:50 +0700

    appa

--- TP_MOD_04/Program.cs
+++ TP_MOD_04/Program.cs
@@ -1,2 +1,65 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Security.Cryptography.X509Certificates;
+
+class KodePos
+{
+    public enum Kelurahan { Batununggal, Kujangsari, Mengger, Wates, Cijaura, Jatisari, Margasari, Sekejati, Kebonwaru, Maleer, Samoja };
+    public static int getKodePos(Kelurahan kelurahan)
+    {
+        if (kelurahan == Kelurahan.Batununggal)
+        {
+            return 40266;
+        }
+        if (kelurahan == Kelurahan.Kujangsari)
+        {
+            return 40287;
+        }
+        if (kelurahan == Kelurahan.Mengger)
+        {
+            return 40267;
+        }
+        if (kelurahan == Kelurahan.Wates)
+        {
+            return 40256;
+        }
+        if (kelurahan == Kelurahan.Cijaura)
+        {
+            return 40287;
+        }
+        if (kelurahan == Kelurahan.Jatisari)
+        {
+            return 40286;
+        }
+        if (kelurahan == Kelurahan.Margasari)
+        {
+            return 40286;
+        }
+        if (kelurahan == Kelurahan.Sekejati)
+        {
+            return 40286;
+        }
+        if (kelurahan == Kelurahan.Kebonwaru)
+        {
+            return 40272;
+        }
+        if (kelurahan == Kelurahan.Maleer)
+        {
+            return 40274;
+        }
+        if (kelurahan == Kelurahan.Samoja)
+        {
+            return 40273;
+        }
+        return 0;
+    }
+}
+
+class main
+{
+    static void Main(String[] args)
+    {
+
+        KodePos kodePos = new KodePos();
+        kodePos.getKodePos();
+    }
+}
--- TP_MOD_04/obj/Debug/net8.0/TP_MOD_04.AssemblyInfo.cs
+++ TP_MOD_04/obj/Debug/net8.0/TP_MOD_04.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP_MOD_04")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bbef8f8ed65ccc42dde272f4fde3cbeb5921df9")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP_MOD_04")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP_MOD_04")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
